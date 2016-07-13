b17683236772e536cb5ba320fd407a437079963e
Justin Doak <justinhdoak@gmail.com>
2016-07-13 12:50:29 -0400

    updated css and javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,30 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+// Defining gameBoard variable and looping to create 4 card divs
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+};
+
+createBoard();
+
+
+
+
+
+
+
+
+/*
 if (cardTwo === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
 }
+*/
